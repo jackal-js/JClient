78b7055f342e80b2de0ab40a5223187663032ffb
Blake <blake.needleman@gmail.com>
2013-11-20 12:17:01 -0500

    Updated console

--- console.js
+++ console.js
@@ -1,345 +1,133 @@
-/**
- */
-require(['knockout-2.2.1',
-        'parse/parse',
-        'nu/stream',
-        'ecma/lex/lexer',
-        'ecma/parse/parser',
-        'sheut/run',
-        'sheut/operations/context',
-        'sheut/operations/reference',
-        'sheut/operations/evaluation',
-        'sheut/debug',
-        'sheut/state',
-        'sheut/step',],
-function(ko,
-        parse,
-        stream,
-        lexer,
-        parser,
-        run,
-        context,
-        reference,
-        evaluation,
-        debug,
-        state,
-        step) {
-
-var reduce = Function.prototype.call.bind(Array.prototype.reduce);
-
-var get = function(p, c) {
-    return p[c];
-};
-
-/* 
- ******************************************************************************/
-var printBindings = function(d, record) {
-    if (record.ref) {
-        var obj = run.extract(d, reference.getValue(record), null);
-        record = obj.properties;
-    }
-    return Object.keys(record).reduce(function(p, c) {
-        p.push({'name': c, 'value': run.extract(d, reference.getValue(record[c]), null)});
-        return p;
-    }, []);
-};
-
-
-var printFrame = function(d, lex) {
-    return {
-        'bindings': printBindings(d, lex.record)
-    };
-};
-
-var printEnvironments = function(d) {
-    var environments = [];
-    var environment = run.extract(d, context.environment, null);
-    do {
-        environments.push(printFrame(d, environment));
-        environment = run.extract(d, reference.getValue(environment.outer), null);
-    } while (environment);
-    return environments;
-};
-
-
-/* 
- ******************************************************************************/
-var out = {
-    'write': function(x, ctx) {
-        model.push(x, ctx, false);
-    }
-};
-
-var errorOut = {
-    'write': function(x, ctx) {
-        model.push(x, ctx, true);
-    }
-};
-
-/* Code Mirror
- ******************************************************************************/
-
-
-var codeMirror = CodeMirror(document.getElementById('input'), {
-    'mode': 'javascript',
-    'lineNumbers': true
-});
-
-var doc = codeMirror.doc;
-
-console.log("load codemirror");
-
-var interactive = CodeMirror(document.getElementById('output-interactive-textarea'), {
-    'mode': 'javascript',
-    'lineNumbers': false,
-});
-interactive.setSize(null, 20);
-interactive.on('beforeChange', function(instance, change) {
-    change.update(change.from, change.to, [change.text.join("").replace(/\n/g, "")]);
-    return true;
-});
-
-interactive.on('keyHandled', function(instance, name, event) {
-    if (name === 'Enter') {
-        runContext(interactiveDoc.getValue(), model.debug().ctx, out.write, errorOut.write);
-    }
-});
-
-var interactiveDoc = interactive.doc;
-
-/* 
- ******************************************************************************/
-var AtumObject = function(d, x, ctx) {
-    var self = this;
-    var value = d.getValue(x, function(x, ctx){ return x; }, function(x, ctx){ return x; });
-    
-    self.value = ko.observable(value);
-    self.children = ko.observableArray();
-    
-    self.getChildren = function(data) {
-        var value = data.value().value();
-        if (data.value().children().length === 0) {
-            if (value && value.type && value.type === 'object') {
-                Object.keys(value.properties).map(function(key) {
-                    data.value().children.push(
-                        new AtumChild(key, new AtumObject(d, value.properties[key].value, ctx)));
-                });
-            }
-        }
-        $('.object-browser').accordion()
-            .accordion('refresh');
-    };
-};
-
-var AtumChild = function(key, value) {
-    var self = this;
-
-    self.key = ko.observable(key);
-    self.value = ko.observable(value);
-};
-
-/* ConsoleViewModel
- ******************************************************************************/
-var ConsoleViewModel = function() {
-    var self = this;
-    
-    this.debug = ko.observable();
-    
-    this.output = ko.observableArray();
-    
-    this.environments = ko.computed(function(){
-        return (self.debug() ?
-            printEnvironments(self.debug()) :
-            []);
+/*
+ * THIS FILE IS AUTO GENERATED from 'lib/console.kep'
+ * DO NOT EDIT
+*/
+define(["require", "exports", "knockout-2.2.1", "sheut/debug", "sheut/step", "sheut/run", "sheut/operations/evaluation", "atum_debug_console/model", "atum_debug_console/object_explorer"], (function(require, exports, ko, debug, step, run, evaluate, atum_debug_model, object_explorer) {
+    "use strict";;
+    var ko = ko,
+        debug = debug,
+        step = step,
+        run = run,
+        evaluate = evaluate,
+        atum_debug_model = atum_debug_model,
+        object_explorer = object_explorer;;;;
+    var id = (function(x) {
+        return x;
     });
-    
-    this.location = ko.computed((function() {
-        return (self.debug() ? run.extract(self.debug(), context.location, null) : []);
-    }));
-    
-    this.stack = [];/*ko.computed(function(){
-        return (self.debug() && self.debug().ctx.userData ? 
-            ko.utils.arrayMap(self.debug().ctx.userData.metadata.stack, function(x) {
-                return {
-                    'name': (x.func ? self.debug().run(object.get(x.func, 'name'), function(x){ return x.value; }) : '')
-                };
-            }) :
-            [])
-    });*/
-};
-
-ConsoleViewModel.prototype.finish = function() {
-    return this.debug(step.finish(this.debug()));
-};
-
-ConsoleViewModel.prototype.run = function() {
-    return this.debug(step.run(this.debug()));
-};
-
-ConsoleViewModel.prototype.stepOver = function() {
-    return this.debug(step.stepOver(this.debug()));
-};
-
-ConsoleViewModel.prototype.stepInto = function() {
-    return this.debug(step.step(this.debug()));
-};
-
-ConsoleViewModel.prototype.stepOut = function() {
-    return this.debug(step.stepOut(this.debug()));
-};
-
-ConsoleViewModel.prototype.push = function(value, ctx, error) {
-   // var obj = new AtumObject(atum_debugger.Debugger.create(compute.just(value), ctx, interpret.noop, interpret.noop), value, ctx);
-    //obj.getChildren({'key':'', 'value': ko.observable(obj) });
-    this.output.push({
-        'value': value,
-        'error': !!error
-    });
-    return this;
-};
-
-ConsoleViewModel.prototype.stop = function() {
-    return this.debug(null);
-};
-
-
-
-/* 
- ******************************************************************************/
-var model = new ConsoleViewModel();
-ko.applyBindings(model);
-
-model.location.subscribe((function(current){
-    if((current && current.start)) codeMirror.removeLineClass((current.start.line - 1), "background", "active-line");
-}), null, "beforeChange");
-
-model.location.subscribe((function(x) {
-    if((x && x.start)) codeMirror.addLineClass((x.start.line - 1), "background", "active-line");
-}));
-
-$(function(){
-    var stopButton = $('button#stop-button'),
-        runButton = $('button#run-button'),
-        stepOverButton = $('button#step-over-button'),
-        stepOutButton = $('button#step-out-button'),
-        stepIntoButton = $('button#step-into-button');
-    
-    $('#container').layout();
-    
-    $('#refresh-button').click(function(){
-       doc.setValue(codeMirrorText); 
-    });
-    
-    $('#save-button').click(function(){
-        $('#saveModal').modal('show');
-        $('#saveText').text(doc.getValue());
-        if($('#saveTitle').val() == ''){
-            $('#sessionFlag').text('New');
-        }else{
-            $('#sessionFlag').text('Save');
-        }
-    });
-    
-    $('#fork-button').click(function(){
-       $('#saveModal').modal('show');
-       $('#saveText').text(doc.getValue()).attr("disabled", true);
-       $('#saveTitle').attr("disabled", true);
-       $('#sessionFlag').text('Fork');
-    });
-    
-    $('#share-button')
-        .click(function(){
-            var url = window.location.href.split('?');
-            $('#shareLink').val(url[0].concat("?").concat(url[1]).concat("?").concat(url[2]));
-            $('#shareModal').modal('show');
-        });
-    
-    $('button#eval-button')
-        .button()
-        .click(function(e){
-            $('#output-console').text("");
-           // run(doc.getValue(), out.write, errorOut.write);
-            step.finish(debug.beginInput(doc.getValue(),
-                    out.write,
-                    errorOut.write));
-            $('.object-browser')
-                .accordion({
-                    'collapsible': true,
-                    'animate': 100
-                });
-        });
-    
-    $('.object-browser')
-        .accordion();
-    
-    $('button#debug-button')
-        .button()
-        .click(function () {
-            var input = doc.getValue();
-            
-            try {
-                
-                //var ast = parser.parse(input);
-                //var p = semantics.programBody(semantics.sourceElements(ast.body));
-                
-                //var ctx = globalCtx;
-                model.debug(debug.beginInput(input,
-                    out.write,
-                    errorOut.write));
-                    
-                stopButton.attr("disabled", false);
-                runButton.attr("disabled", false);
-                stepOverButton.attr("disabled", false);
-                stepIntoButton.attr("disabled", false);
-                stepOutButton.attr("disabled", false);
-                $('.output-value').text("");
-                $('.ParseError').text("");
-            } catch (e) {
-                $('.ParseError').text(e);
-                throw e;
-            }
-        });
-    
-    stopButton
-        .button()
-        .attr("disabled", true)
-        .click(function(e){
-            model.stop();
+    var model;
+    var out = ({
+        "write": (function(x, ctx) {
+            model.pushResult(x, ctx, false);
+            $("#output > ul").scrollTop($("#output > ul")[0].scrollHeight);
         })
+    });
+    var errorOut = ({
+        "write": (function(x, ctx) {
+            model.pushResult(x, ctx, true);
+            $("#output > ul").scrollTop($("#output > ul")[0].scrollHeight);
+        })
+    });
+    var cm = CodeMirror(document.getElementById("input"), ({
+        "mode": "javascript",
+        "lineNumbers": true
+    }));
+    var doc = cm.doc;
+    var interactive = CodeMirror(document.getElementById("output-interactive-textarea"), ({
+        "mode": "javascript",
+        "lineNumbers": false
+    }));
+    var interactiveDoc = interactive.doc;
+    interactive.setSize(null, 20);
+    interactive.on("beforeChange", (function(instance, change) {
+        change.update(change.from, change.to, [change.text.join("").replace(/\n/g, "")]);
+        return true;
+    }));
+    interactive.on("keyHandled", (function(instance, name, event) {
+        if ((name === "Enter")) {
+            var input = interactiveDoc.getValue(); {
+                var writeOut = (function(x, ctx) {
+                    interactive.setValue("");
+                    model.pushInput(input);
+                    out.write(x, ctx);
+                }),
+                    writeError = (function(x, ctx) {
+                        interactive.setValue("");
+                        model.pushInput(input);
+                        errorOut.write(x, ctx);
+                    });
+                if (model.debug()) {
+                    run.evaluate(model.debug(), evaluate.evaluateInput(input), writeOut, writeError);
+                } else {
+                    step.finish(debug.beginInput(input, writeOut, writeError));
+                }
 
-    runButton
-        .button()
-        .attr("disabled", true)
-        .click(function(e) {
-            $('.output-value').text("");
-            $('.ParseError').text("");
-            if (model.debug()) {
-                model.run();
-            } else {
-                model.finish();
             }
-        });
-    
-    stepOverButton
-        .button()
-        .attr("disabled", true)
-        .click(function(e){
-            model.stepOver();
-        });
-    
-    stepIntoButton
-        .button()
-        .attr("disabled", true)
-        .click(function(e){
-            model.stepInto();
-        });
-    
-    stepOutButton
-        .button()
-        .attr("disabled", true)
-        .click(function(e){
-            model.stepOut();
-        });
-});
+        }
 
-});
+    }));
+    (model = new(atum_debug_model.ConsoleViewModel)());
+    ko.applyBindings(model);
+    model.location.subscribe((function(current) {
+        if ((current && current.start)) cm.removeLineClass((current.start.line - 1), "background", "active-line");
+
+    }), null, "beforeChange");
+    model.location.subscribe((function(x) {
+        if ((x && x.start)) cm.addLineClass((x.start.line - 1), "background", "active-line");
+
+    }));
+    $((function() {
+        var stopButton = $("button#stop-button"),
+            runButton = $("button#run-button"),
+            stepButton = $("button#step-button"),
+            stepOutButton = $("button#step-out-button"),
+            stepIntoButton = $("button#step-into-button"),
+            stepOverButton = $("button#step-over-button");
+        $("#container").layout();
+        model.debug.subscribe((function(x) {
+            var disable = (!x || x.debug.complete);
+            stopButton.button("option", "disabled", disable);
+            runButton.button("option", "disabled", disable);
+            stepButton.button("option", "disabled", disable);
+            stepOverButton.button("option", "disabled", disable);
+            stepIntoButton.button("option", "disabled", disable);
+            stepOutButton.button("option", "disabled", disable);
+        }));
+        $("#output > ul").on("accordionactivate", ".object-browser", (function(event, ui) {
+            return $("#output > ul").scrollTop($(this).offset().top);
+        }));
+        $("button#eval-button").button().click((function(e) {
+            var input = doc.getValue();
+            model.debug(debug.beginInput(input, out.write, errorOut.write));
+            model.finish();
+        }));
+        $("button#debug-button").button().click((function(e) {
+            var input = doc.getValue();
+            model.debug(debug.beginInput(input, out.write, errorOut.write));
+        }));
+        stopButton.button(({
+            "disabled": true
+        })).click((function(e) {
+            model.stop();
+        }));
+        runButton.button(({
+            "disabled": true
+        })).click((function(e) {
+            model.run();
+        }));
+        stepButton.button(({
+            "disabled": true
+        })).click((function(e) {
+            model.stepOver();
+        }));
+        stepIntoButton.button(({
+            "disabled": true
+        })).click((function(e) {
+            model.stepInto();
+        }));
+        stepOutButton.button(({
+            "disabled": true
+        })).click((function(e) {
+            model.stepOut();
+        }));
+    }));
+}))
---
+++ model.js
@@ -0,0 +1,117 @@
+/*
+ * THIS FILE IS AUTO GENERATED from 'lib/model.kep'
+ * DO NOT EDIT
+*/
+define(["require", "exports", "knockout-2.2.1", "amulet/record", "atum/compute", "atum/operations/object", "sheut/debug", "sheut/run", "sheut/step", "sheut/operations/context", "sheut/operations/reference", "atum_debug_console/object_explorer"], (function(require, exports, ko, record, compute, object, debug, run, step, context, reference, object_explorer) {
+    "use strict";
+    var Result, Input, ConsoleViewModel;
+    var ko = ko,
+        record = record,
+        compute = compute,
+        object = object,
+        debug = debug,
+        run = run,
+        step = step,
+        context = context,
+        reference = reference,
+        object_explorer = object_explorer,
+        AtumObject = object_explorer["AtumObject"];;
+    var map = (function(f, a) {
+        return Array.prototype.map.call(a, f);
+    });
+    var noop = (function(x) {
+        return x;
+    });
+    (Result = record.declare(null, ["value", "error"]));
+    (Result.prototype.type = "result");
+    (Input = record.declare(null, ["input"]));
+    (Input.prototype.type = "input");
+    var Environment = (function(bindings) {
+        (this.bindings = ko.observableArray(bindings));
+    });
+    var Binding = (function(name, value) {
+        (this.name = name);
+        (this.value = value);
+    });
+    var printBindings = (function(d, environment) {
+        return (function() {
+            {
+                var bindings = run.extract(d, context.getEnvironmentBindings(environment), []);
+                return bindings.reduce((function(p, c) {
+                    p.push(new(Binding)(c, run.extract(d, context.getEnvironmentBinding(environment, c), null, (function(x, ctx) {
+                        return object_explorer.createForKey(c, x, ctx);
+                    }))));
+                    return p;
+                }), []);
+            }
+        })();
+    });
+    var printFrame = (function(d, lex) {
+        return new(Environment)(printBindings(d, lex));
+    });
+    var printEnvironments = (function(d) {
+        var environments = [];
+        var environment = run.extract(d, context.environment, null);
+        do {
+            environments.push(printFrame(d, environment));
+            (environment = run.extract(d, context.getEnvironmentOuter(environment), null));
+        }
+        while (environment);
+        return environments;
+    });
+    var StackFrame = (function(name) {
+        (this.name = name);
+    });
+    var createStackFrame = (function(d, frame) {
+        return new(StackFrame)(run.extract(d, context.getStackFrameName(frame), "", (function(x) {
+            return x.value;
+        })));
+    });
+    var printStack = (function(d) {
+        return run.extract(d, context.stack, [], map.bind(null, createStackFrame.bind(null, d)));
+    });
+    (ConsoleViewModel = (function() {
+        var self = this;
+        (self.debug = ko.observable());
+        (self.output = ko.observableArray());
+        (self.location = ko.computed((function() {
+            return (self.debug() ? run.extract(self.debug(), context.location, null) : []);
+        })));
+        (self.environments = ko.computed((function() {
+            return (self.debug() ? printEnvironments(self.debug()) : []);
+        })));
+        (self.stack = ko.computed((function() {
+            return (self.debug() ? printStack(self.debug()) : []);
+        })));
+    }));
+    (ConsoleViewModel.prototype.finish = (function() {
+        return this.debug(step.finish(this.debug()));
+    }));
+    (ConsoleViewModel.prototype.run = (function() {
+        return this.debug(step.run(this.debug()));
+    }));
+    (ConsoleViewModel.prototype.stepOver = (function() {
+        return this.debug(step.stepOver(this.debug()));
+    }));
+    (ConsoleViewModel.prototype.stepInto = (function() {
+        return this.debug(step.step(this.debug()));
+    }));
+    (ConsoleViewModel.prototype.stepOut = (function() {
+        return this.debug(step.stepOut(this.debug()));
+    }));
+    (ConsoleViewModel.prototype.stop = (function() {
+        return this.debug(null);
+    }));
+    (ConsoleViewModel.prototype.pushResult = (function(value, ctx, error) {
+        return this.output.push(Result.create(object_explorer.create(value, ctx), error));
+    }));
+    (ConsoleViewModel.prototype.pushInput = (function(input) {
+        return this.output.push(Input.create(input));
+    }));
+    (ConsoleViewModel.prototype.selectFrame = (function(item) {
+        this.environment();
+    }));
+    (exports.Result = Result);
+    (exports.Input = Input);
+    (exports.ConsoleViewModel = ConsoleViewModel);
+}))
